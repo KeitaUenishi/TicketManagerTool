ea030e587b459cadebebcd7e0a54ddf7569be002
github KeitaUenishi <keita.uenishi@gmail.com>
2020-11-29 12:08:43 +0900

    作成時デフォルト

---
+++ src/main/java/product/TicketManegerToolApplication.java
@@ -0,0 +1,13 @@
+package product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketManegerToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketManegerToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/product/TicketManegerToolApplicationTests.java
@@ -0,0 +1,13 @@
+package product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketManegerToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
