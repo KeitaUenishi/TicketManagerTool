174713fdb9df337177859371ea104c53dd57a408
github KeitaUenishi <keita.uenishi@gmail.com>
2020-12-21 22:39:54 +0900

    初期表示データ追加

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO live_list VALUES(20201210, 'アメリカ村', 'キャパ10');
+INSERT INTO live_list VALUES(20210105, '東京エジプト', 'ノルマ15');
+
+INSERT INTO customers_list VALUES(NULL, 20201210, '田中権蔵', 1, '最近来た');
+INSERT INTO customers_list VALUES(NULL, 20210105, '御手洗俊五郎', 3, 'たくさんくる');
